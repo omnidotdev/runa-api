812f741cd7a49aa1cef68a2180c5778242ceab45
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-07-21 19:08:24 -0500

    chore: prep merge

--- src/lib/db/drizzle/0010_free_spencer_smythe.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "project" DROP CONSTRAINT "project_workspace_id_workspace_id_fk";
---> statement-breakpoint
-ALTER TABLE "project" ADD CONSTRAINT "project_workspace_id_workspace_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "public"."workspace"("id") ON DELETE cascade ON UPDATE no action;
