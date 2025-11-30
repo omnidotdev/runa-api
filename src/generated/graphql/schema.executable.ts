7e5cd780ba294882d8601d3bdbfb4c5bd7397c47
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-30 14:14:25 -0600

    build(deps): remove polar dependencies and adjust accordingly

--- src/generated/graphql/schema.executable.ts
+++ src/generated/graphql/schema.executable.ts
@@ -2,7 +2,6 @@
 import { PgBooleanFilter, PgCondition, PgDeleteSingleStep, PgExecutor, PgOrFilter, TYPES, assertPgClassSingleStep, enumCodec, listOfCodec, makeRegistry, pgDeleteSingle, pgInsertSingle, pgSelectFromRecord, pgUpdateSingle, pgWhereConditionSpecListToSQL, recordCodec, sqlValueWithCodec } from "@dataplan/pg";
 import { ConnectionStep, EdgeStep, ExecutableStep, Modifier, ObjectStep, __ValueStep, access, assertExecutableStep, bakedInputRuntime, connection, constant, context, createObjectAndApplyChildren, first, get as get2, inhibitOnNull, inspect, isExecutableStep, lambda, list, makeDecodeNodeId, makeGrafastSchema, object, rootValue, sideEffect, specFromNodeId } from "grafast";
 import { GraphQLError, Kind } from "graphql";
-import { polar } from "lib/polar/sdk";
 import { sql } from "pg-sql2";
 import { match } from "ts-pattern";
 const nodeIdHandler_Query = {
@@ -6534,9 +6533,6 @@ const planWrapper14 = (plan, _, fieldArgs) => {
       });
       if (!workspace || !workspace.workspaceUsers.length) throw Error("Unauthorized");
       if (workspace.workspaceUsers[0].role !== "owner") throw Error("Unauthorized");
-      if ("create" === "delete" && !!workspace.subscriptionId) await polar.subscriptions.revoke({
-        id: workspace.subscriptionId
-      });
     }
   });
   return plan();
@@ -7344,9 +7340,6 @@ const planWrapper28 = (plan, _, fieldArgs) => {
       });
       if (!workspace || !workspace.workspaceUsers.length) throw Error("Unauthorized");
       if (workspace.workspaceUsers[0].role !== "owner") throw Error("Unauthorized");
-      if ("update" === "delete" && !!workspace.subscriptionId) await polar.subscriptions.revoke({
-        id: workspace.subscriptionId
-      });
     }
   });
   return plan();
@@ -8154,9 +8147,6 @@ const planWrapper42 = (plan, _, fieldArgs) => {
       });
       if (!workspace || !workspace.workspaceUsers.length) throw Error("Unauthorized");
       if (workspace.workspaceUsers[0].role !== "owner") throw Error("Unauthorized");
-      if ("delete" === "delete" && !!workspace.subscriptionId) await polar.subscriptions.revoke({
-        id: workspace.subscriptionId
-      });
     }
   });
   return plan();
--- src/lib/graphql/plugins/authorization/Workspace.plugin.ts
+++ src/lib/graphql/plugins/authorization/Workspace.plugin.ts
@@ -2,14 +2,12 @@ import { EXPORTABLE } from "graphile-export";
 import { context, sideEffect } from "postgraphile/grafast";
 import { wrapPlans } from "postgraphile/utils";
 
-import { polar } from "lib/polar/sdk";
-
 import type { PlanWrapperFn } from "postgraphile/utils";
 import type { MutationScope } from "./types";
 
 const validatePermissions = (propName: string, scope: MutationScope) =>
   EXPORTABLE(
-    (context, sideEffect, polar, propName, scope): PlanWrapperFn =>
+    (context, sideEffect, propName, scope): PlanWrapperFn =>
       (plan, _, fieldArgs) => {
         const $input = fieldArgs.getRaw(["input", propName]);
         const $observer = context().get("observer");
@@ -35,21 +33,12 @@ const validatePermissions = (propName: string, scope: MutationScope) =>
 
             // TODO: determine proper permissions for admins when it comes to updating / deleting workspaces
             if (role !== "owner") throw new Error("Unauthorized");
-
-            // TODO: figure out best way to make subscriptionId not nullable, *or* update logic to possibly throw if subId is null
-            if (scope === "delete" && !!workspace.subscriptionId) {
-              await polar.subscriptions.revoke({
-                id: workspace.subscriptionId,
-              });
-
-              // TODO: determine how to handle errors from above.
-            }
           }
         });
 
         return plan();
       },
-    [context, sideEffect, polar, propName, scope],
+    [context, sideEffect, propName, scope],
   );
 
 export default wrapPlans({
